a13d156eef2486d8052912a4bd15ad4b240b7372
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-30 15:11:33 -0400

    Errors working and render to screen - yippeee

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,27 +2,42 @@ class PokemonController < ApplicationController
 
 def show
 
-
-pokemon = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
-pokemon_body = JSON.parse(pokemon.body)
-
-if pokemon.code == 404
-  render :error
-else
-gif = Typhoeus.get("http://api.giphy.com/v1/gifs/search?q=#{pokemon_body["name"]}&api_key=#{ENV['GIPHY_KEY']}&limit=1", followlocation: true)
-gif_body = JSON.parse(gif.body)
+pokemon = Typhoeus::Request.new("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
 
 
+    pokemon.on_complete do |response|
+
+      if response.success?
+        pokemon_body = JSON.parse(response.body)
+
+        gif = Typhoeus.get("http://api.giphy.com/v1/gifs/search?q=#{pokemon_body["name"]}&api_key=#{ENV['GIPHY_KEY']}&limit=1", followlocation: true)
+        gif_body = JSON.parse(gif.body)
+
+           render json: {
+             name: pokemon_body["name"],
+             id: pokemon_body["id"],
+             types: pokemon_body["types"][0]["type"]["name"],
+             weight: pokemon_body["weight"],
+             gif:  gif_body["data"][0]["url"]
+          }
+
+      elsif response.timed_out?
+        # aw hell no
+         log("got a time out")
+      elsif response.code == 0
+        # Could not get an http response, something's wrong.
+        log(response.return_message)
+      else
+        # Received a non-successful http response.
+
+        render json: { ERROR: "HTTP request failed: " + response.code.to_s}
+      end
+    end
+
+    pokemon.run
 
 
-   render json: {
-     name: pokemon_body["name"],
-     id: pokemon_body["id"],
-     types: pokemon_body["types"][0]["type"]["name"],
-     weight: pokemon_body["weight"],
-     gif:  gif_body["data"][0]["url"]
-  }
+
 end
 
-end
 end
